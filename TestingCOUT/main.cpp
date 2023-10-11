8985cf146172f57f50f081816ffbf70a3bf338fa
Hayden Williams <haydencwilliams26@gmail.com>
2023-10-11 09:55:22 -0400

    Initial Commit

---
+++ TestingCOUT/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TestingCOUT
+//
+//  Created by HAYDEN WILLIAMS on 10/11/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
