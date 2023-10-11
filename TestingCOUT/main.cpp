f7f0f0bd52b05042f2fea59dc4929f2ea2393beb
Hayden Williams <haydencwilliams26@gmail.com>
2023-10-11 10:07:33 -0400

    Added functionality and doc box

--- TestingCOUT/main.cpp
+++ TestingCOUT/main.cpp
@@ -1,9 +1,12 @@
-//
-//  main.cpp
-//  TestingCOUT
-//
-//  Created by HAYDEN WILLIAMS on 10/11/23.
-//
+// NAME                 : Hayden Williams
+// GROUP                : Argon
+// LAST MODIFIED        : 10/11/23
+// PROBLEM ID           : Dietal 1.24 Stream Operator Exercise
+// PROBLEM DESCRIPTION  : Using stream operators to print out a message in 3 different ways.
+// SOURCES              : Mr. Houtrouw
+// PEOPLE WHO HELPED    : Mr. Houtrouw
+// PEOPLE WHO I HELPED  : Brooke Winters
+
 
 #include <iostream>
 
